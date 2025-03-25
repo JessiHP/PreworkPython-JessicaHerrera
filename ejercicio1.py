8a52dd693d0ddcd9e6e9b72c67320a920eeacbec
JessiHP <jessicam.hrra@gmail.com>
2025-03-25 13:00:38 +0100

    eje1_finalizado

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,21 +1,19 @@
 
-#Variables y Operadores#
+# Variables y Operadores #
 
 '''
 1. Ejercicio: Crea una variable llamada nombre y asígnale tu nombre como valor.
 Luego, imprime la variable.
 '''
-
 nombre = 'Jessica'
 print(nombre)
 
 '''
-2. Ejercicio: Crea dos variables, a y b , y asígnales los valores 5 y 10
-respectivamente. Luego, imprime la suma de a y b .
+2. Ejercicio: Crea dos variables, a y b , y asígnales los valores 5 y 10 respectivamente.
+Luego, imprime la suma de a y b .
 '''
 a = 5
 b = 10
-
 print(a+b)
 
 '''
@@ -34,7 +32,7 @@ resto = 17 % 3
 print (resto)
 
 
-##otra forma##
+## Otra forma ##
 division = int (17/3)
 print(division)
 resto2 = 17-(division * 3)
